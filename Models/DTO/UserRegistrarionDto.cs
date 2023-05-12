34299dbac97ef5aab6e2500a4dcedd6642bede4a
DrTau <drttau@gmail.com>
2023-05-12 22:07:33 +0300

    Add min leanth to regDto.

--- Models/DTO/UserRegistrarionDto.cs
+++ Models/DTO/UserRegistrarionDto.cs
@@ -4,11 +4,11 @@ namespace BetMe.Models
 {
     public class UserRegistrationDto
     {
-        [Required, MaxLength(64)]
+        [Required, MaxLength(64), MinLength(3)]
         public string Name { get; set; } = string.Empty;
         [Required, MaxLength(128), EmailAddress]
         public string Email { get; set; } = string.Empty;
-        [Required, MaxLength(64)]
+        [Required, MaxLength(64), MinLength(8)]
         public string Password { get; set; } = string.Empty;
     }
 }
